68b6802808830b66635cefeabb7132101408845a
Marcelo Delgado <mdj.delgado87@gmail.com>
2020-06-28 19:22:12 -0500

    Update GP_register_TB.vhd

--- GP_Register/GP_register_TB.vhd
+++ GP_Register/GP_register_TB.vhd
@@ -1,35 +1,89 @@
+--------------------------------------------------------------------------------
+--  Name: GP_register_TB.vhd
+--  Dependencies: GP_register.vhd
+--  Design Software: Vivado 2018.3
+--  Engineer: Marcelo Delgado
+--  Explanation video: https://www.youtube.com/watch?v=wGUggIjnRyo
+--
+--  Description:
+--  Testbench file for general purpose register
+--
+--  Version History:
+--  1.0 - 28/06/2020 Marcelo Delgado
+--    Initial Public Release
+--------------------------------------------------------------------------------
+
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.Std_logic_1164.all;
+use IEEE.Numeric_Std.all;
 
-entity GP_register is
-  port(SI : in std_logic;
-       B : in std_logic_vector(3 downto 0);
-       n_load, n_clear : in std_logic;
-       clk : in std_logic;
-       Q : out std_logic_vector(3 downto 0);
-       SO : out std_logic );
-end GP_register;
+entity GP_register_TB is
+end;
 
-architecture Behavioral of GP_register is
+architecture bench of GP_register_TB is
 
-signal Q_s : std_logic_vector(3 downto 0);
+  component GP_register
+    port(SI : in std_logic;
+         B : in std_logic_vector(3 downto 0);
+         n_load, n_clear : in std_logic;
+         clk : in std_logic;
+         Q : out std_logic_vector(3 downto 0);
+         SO : out std_logic );
+  end component;
+
+  signal SI: std_logic;
+  signal B: std_logic_vector(3 downto 0);
+  signal n_load, n_clear: std_logic;
+  signal clk: std_logic;
+  signal Q: std_logic_vector(3 downto 0);
+  signal SO: std_logic ;
+
+  constant clock_period: time := 10 ns;
+  signal stop_the_clock: boolean := FALSE;
 
 begin
 
-   reg: process(clk,n_clear)
-	begin
-	   if (n_clear='0') then
-	      Q_s <= "0000";
-	   elsif (rising_edge(CLK)) then
-	      if (n_load='0') then
-	         Q_s <= B;
-	      else
-	         Q_s <= SI & Q_s(3 downto 1);
-	      end if;
-	   end if;
-	end process;
-	
-	Q <= Q_s;
-	SO <= Q_s(0);
+  uut: GP_register port map ( SI      => SI,
+                              B       => B,
+                              n_load  => n_load,
+                              n_clear => n_clear,
+                              clk     => clk,
+                              Q       => Q,
+                              SO      => SO );
 
-end Behavioral;
+  stimulus: process
+  begin
+  
+    -- Initial values
+    B <= "0000";
+    n_load <= '1';
+    n_clear <= '0';
+    SI <= '1';
+    
+    wait for 20ns;
+    
+    -- Disable clear
+    n_clear <= '1';
+    
+    wait for 50ns;
+    
+    -- Load 1010
+    B <= "1010";
+    n_load <= '0';
+    wait for 20ns;
+    n_load <= '1';
+    
+    wait;
+    
+  end process;
+
+  clocking: process
+  begin
+    while not stop_the_clock loop
+      CLK <= '0', '1' after clock_period / 2;
+      wait for clock_period;
+    end loop;
+    wait;
+  end process;
+
+end;
